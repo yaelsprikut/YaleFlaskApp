8bcab269651072ceeded3f499b449bc83d0f4071
yaelsprikut <ysprikut@gmail.com>
2018-05-02 01:27:03 -0400

    crazy changes man

--- app.py
+++ app.py
@@ -1,12 +1,39 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, flash, request
+from wtforms import Form, TextField, validators
+import requests
+from bs4 import BeautifulSoup
 
 
 app = Flask(__name__)
+app.config.from_object(__name__)
+
+app.config['SECRET_KEY'] = '7d441f27d441f27567d441f2b6176a'
 
 
-@app.route('/')
+class ReusableForm(Form):
+    email = TextField('Enter your shared Spotify or Apple Music URI:')
+
+
+@app.route("/", methods=['GET', 'POST'])
 def index():
-        return render_template('index.html')
+    form = ReusableForm(request.form)
+    print form.errors
+    if request.method == 'POST':
+        example_input_email = request.form['exampleInputEmail1']
+        # print example_input_email
+        page = requests.get(example_input_email)
+        soup = BeautifulSoup(page.content, 'html.parser')
+        data = soup.find('title').get_text()
+        print data
+
+
+        if form.validate():
+            # Save the comment here.
+            flash('Your song is  ' + data)
+        else:
+            flash('Error: All the form fields are required. ')
+
+    return render_template('index.html', form=form)
 
 
 @app.route('/about')
--- spotify.py
+++ spotify.py
@@ -4,7 +4,10 @@ import re
 from bs4 import BeautifulSoup
 
 page = requests.get("https://open.spotify.com/track/1fOkmYW3ZFkkjIdOZSf596?si=-8fZvB7HRuSTw_Moftl8ug")
+apple_page = requests.get("https://itunes.apple.com/ca/album/sour/899380254?i=899380279")
 soup = BeautifulSoup(page.content, 'html.parser')
+
+apple_soup = BeautifulSoup(apple_page.content, 'html.parser')
 
 pattern = re.compile(r"Spotify.Entity\s+=\s+(\{.*?\});\n")
 script = soup.find("script", text=pattern)
@@ -13,6 +16,10 @@ data = pattern.search(script.text).group(1)
 data = json.loads(data)
 print('The artist name is: '+ data['artists'][0]['name'])
 print('The song name is: '+ data['name'])
+
+print('----------------------------------------------------')
+print(apple_soup.find('title').get_text())
+print(soup.find('title').get_text())
 
 # containers = soup.find_all("script")
 
