5ab80b5282ca49b1935eef16af6141862522167c
yaelsprikut <ysprikut@gmail.com>
2018-05-08 21:05:35 -0400

    try again

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, flash, request
-# from wtforms import Form, StringField
-# import requests
-# from bs4 import BeautifulSoup
+from wtforms import Form, StringField
+import requests
+from bs4 import BeautifulSoup
 
 
 app = Flask(__name__)
@@ -10,13 +10,29 @@ app.config.from_object(__name__)
 app.config['SECRET_KEY'] = '7d441f27d441f27567d441f2b6176a'
 
 
-# class ReusableForm(Form):
-#    email = StringField('Enter your shared Spotify or Apple Music URI:')
+class ReusableForm(Form):
+    email = StringField('Enter your shared Spotify or Apple Music URI:')
 
 
 @app.route("/", methods=['GET', 'POST'])
 def index():
-  return render_template('index.html')
+    form = ReusableForm(request.form)
+    print(form.errors)
+    if request.method == 'POST':
+        example_input_email = request.form['exampleInputEmail1']
+        # print example_input_email
+        page = requests.get(example_input_email)
+        soup = BeautifulSoup(page.content, 'html.parser')
+        data = soup.find('title').get_text()
+        print(data)
+
+        if form.validate():
+            # Save the comment here.
+            flash('Your song is  ' + data)
+        else:
+            flash('Error: All the form fields are required. ')
+
+    return render_template('index.html', form=form)
 
 
 @app.route('/about')
